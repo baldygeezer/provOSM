0e75c0ee5c2d4b3089aa14988fbfcdc8a4b6edf9
bar1g16 <bar1g16@b59-ecs-l39.ecslabs.soton.ac.uk>
2017-07-08 18:27:48 +0100

    Changed the tags container into a string[2] to hold the attributes as pairs. Changed the order so keys are first

--- src/provOSM/OSM_Way.java
+++ src/provOSM/OSM_Way.java
@@ -6,7 +6,7 @@ import java.util.Map;
 public class OSM_Way extends OSM_Primitive {
 	private String[] mNodes;
 
-	public OSM_Way(String changeSet, String uid, String userName, String timeStamp, String version, ArrayList<String> tags, String[] nodes) {
+	public OSM_Way(String changeSet, String uid, String userName, String timeStamp, String version, ArrayList<String[]> tags, String[] nodes) {
 		super(OSMDataType.WAY, changeSet, uid, userName, timeStamp, version, tags);
 		this.mNodes=nodes;
 	}
