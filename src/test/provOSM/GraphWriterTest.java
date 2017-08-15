b37617724bf0d40184ec81e0131f35b03e7d0397
baldy <b.a.roper@soton.ac.uk>
2017-08-15 20:11:15 +0100

    unit tests fixed testFixture file load

--- src/test/provOSM/GraphWriterTest.java
+++ src/test/provOSM/GraphWriterTest.java
@@ -1,7 +1,8 @@
 package provOSM;
 
-import org.junit.After;
-import org.junit.Before;
+import org.junit.*;
+
+import java.util.ArrayList;
 
 import static org.junit.Assert.*;
 
@@ -9,6 +10,41 @@ import static org.junit.Assert.*;
  * Created by baldy on 15/08/17.
  */
 public class GraphWriterTest {
+
+    private static ArrayList<String[]> tags = new ArrayList<String[]>();
+    private static String[] nodes = {"gegg5", "dsfs"};
+    private static OSM_Primitive[] way1 = {
+            new OSM_Way("2345", "26636", "345", "wibble", "anytime", "1", tags, nodes),
+            new OSM_Way("2345", "26636", "345", "wibble", "anytime", "2", tags, nodes),
+            new OSM_Way("2345", "26636", "2356", "wibble", "anytime", "3", tags, nodes),
+    };
+    private static OSM_Primitive[] way2 = {
+
+            new OSM_Way("2323", "26636", "345", "wibble", "anytime", "1", tags, nodes),
+            new OSM_Way("2323", "26636", "345", "wibble", "anytime", "2", tags, nodes),
+    };
+    private static OSM_Primitive[] way3 = {
+            new OSM_Way("232345", "26636", "345", "wibble", "anytime", "1", tags, nodes),
+            new OSM_Way("232345", "26636", "345", "wibble", "anytime", "2", tags, nodes),
+            new OSM_Way("232345", "26636", "345", "wibble", "anytime", "3", tags, nodes),
+            new OSM_Way("232345", "26636", "345", "wibble", "anytime", "4", tags, nodes)};
+
+    private static OSM_Primitive[][] waylist = {way1, way2, way3};
+    private  static GraphWriter graphWriter;
+
+    @BeforeClass
+    public static void setUpBeforeClass() throws Exception {
+
+
+        String[] s = {"wibble", "woo"};
+        String[] y = {"created by", "woo"};
+        tags.add(s);
+        tags.add(y);
+        graphWriter=new GraphWriter(new OSM_Extractor("testfixture.osm"));
+    }
+
+
+
     @Before
     public void setUp() throws Exception {
     }
@@ -17,4 +53,11 @@ public class GraphWriterTest {
     public void tearDown() throws Exception {
     }
 
+    @org.junit.Test
+    public void createVectorCreatesGraphWithCCorrectNumNodes() throws Exception {
+
+        graphWriter.create_feature(way3, "wibble");
+
+
+    }
 }
