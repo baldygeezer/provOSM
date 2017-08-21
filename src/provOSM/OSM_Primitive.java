7582819f4a95e98e9e52cf4274d1c563ff343499
bar1g16 <b.a.roper@soton.ac.uk>
2017-08-21 18:15:46 +0100

    added flag OSM_Primitive to indicate the target feature, I.E. a fixme tag

--- src/provOSM/OSM_Primitive.java
+++ src/provOSM/OSM_Primitive.java
@@ -5,86 +5,94 @@ import java.util.Iterator;
 import java.util.Map;
 
 public abstract class OSM_Primitive {
-	private OSMDataType mType;
-	private String mId;
-	private String mChangeSet;
-	private String mUid;
-	private String mUserName;
-	private String mTimeStamp;
-	private String mVersion;
-	private ArrayList<String[]> mTags;
+    private OSMDataType mType;
+    private String mId;
+    private String mChangeSet;
+    private String mUid;
+    private String mUserName;
+    private String mTimeStamp;
+    private String mVersion;
+    private boolean mFlag;
+    private ArrayList<String[]> mTags;
 
-	public ArrayList<String[]> getTags() {
-		return mTags;
-	}
+    public ArrayList<String[]> getTags() {
+        return mTags;
+    }
 
-	public OSMDataType getType() {
-		return mType;
-	}
+    public OSMDataType getType() {
+        return mType;
+    }
 
-	public String getChangeSet() {
-		return mChangeSet;
-	}
+    public String getChangeSet() {
+        return mChangeSet;
+    }
 
-	public String getUid() {
-		return mUid;
-	}
+    public String getUid() {
+        return mUid;
+    }
 
-	public String getUserName() {
-		return mUserName;
-	}
+    public String getUserName() {
+        return mUserName;
+    }
 
-	public String getTimeStamp() {
-		return mTimeStamp;
-	}
+    public String getTimeStamp() {
+        return mTimeStamp;
+    }
 
-	public int getVersion() {
-		return Integer.parseInt(mVersion) ;
-	}
+    public int getVersion() {
+        return Integer.parseInt(mVersion);
+    }
 
-	public String getId() {
-		return mId;
-	}
+    public String getId() {
+        return mId;
+    }
 
-	public void setmId(String mId) {
-		this.mId = mId;
-	}
+    public void setmId(String mId) {
+        this.mId = mId;
+    }
 
-	public OSM_Primitive(OSMDataType type, String id, String changeSet, String uid, String userName, String timeStamp, String version, ArrayList<String[]> tags) {
-		this.mId = id;
-		this.mType = type;
-		this.mChangeSet = changeSet;
-		this.mUid = uid;
-		this.mUserName = userName;
-		this.mTimeStamp = timeStamp;
-		this.mVersion = version;
-		this.mTags=tags;
-	}
+    public OSM_Primitive(OSMDataType type, String id, String changeSet, String uid, String userName, String timeStamp, String version, ArrayList<String[]> tags) {
+        this.mId = id;
+        this.mType = type;
+        this.mChangeSet = changeSet;
+        this.mUid = uid;
+        this.mUserName = userName;
+        this.mTimeStamp = timeStamp;
+        this.mVersion = version;
+        this.mTags = tags;
+    }
 
-	@Override
-	public String toString() {
-		String wayString ="id: " + mId + ", Username: "+ mUserName +", User ID: "+ mUid + ", Changeset: " + mChangeSet 
-				+ ", Timestamp: " + mTimeStamp + ", Version: " + mVersion + tagsToString();
-		
-		
-		
-		return wayString;
-	}
-	
-	
-	private String tagsToString(){
-		String tagString = " \n Tags \n ************************ \n";
-		
-		for (String[] s : mTags){
-			
-			tagString += "<tag "+ s[0] +  " " + s[1] + " /> \n";
+    @Override
+    public String toString() {
+        String wayString = "id: " + mId + ", Username: " + mUserName + ", User ID: " + mUid + ", Changeset: " + mChangeSet
+                + ", Timestamp: " + mTimeStamp + ", Version: " + mVersion + tagsToString();
 
-			
-			
-		}
-		
-		tagString+="\n";
-		return tagString;
-	}
-	
+
+        return wayString;
+    }
+
+
+    private String tagsToString() {
+        String tagString = " \n Tags \n ************************ \n";
+
+        for (String[] s : mTags) {
+
+            tagString += "<tag " + s[0] + " " + s[1] + " /> \n";
+
+
+        }
+
+        tagString += "\n";
+        return tagString;
+    }
+
+    public boolean getFlag() {
+        return mFlag;
+    }
+
+    public void setFlag(boolean mFlag) {
+        this.mFlag = mFlag;
+    }
+
+
 }
